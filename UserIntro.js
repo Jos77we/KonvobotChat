a2c026c7f79ba62398bcefd836d4a8c0209c1a15
Jos77we <josemaina207@gmail.com>
2024-08-06 08:04:07 +0300

    Deposit and withdrawal added

--- UserIntro.js
+++ UserIntro.js
@@ -5,6 +5,7 @@ const { MessagingResponse } = require("twilio").twiml;
 const { chainRequest } = require("./CreateAcc.js");
 const axios = require("axios");
 const { initPayment } = require("./Payment.js");
+const { WithdrawDeposit, withdrawDeposit } = require("./WithdrawDeposit.js");
 const router = express.Router();
 router.use(bodyParser.urlencoded({ extended: false }));
 
@@ -33,10 +34,11 @@ function initMessage() {
 }
 
 let userState = {};
-const loginStatus = {}; 
+const loginStatus = {};
 let recipientName = null;
 let assetTransct = null;
 let amountTransct = null;
+let reqChangeAsset = null;
 
 const successRes = async (form) => {
   try {
@@ -58,22 +60,34 @@ const successRes = async (form) => {
 
 async function loginSuccess(paymail, password) {
   try {
-    const response = await axios.post("https://api.kipaji.app/api/v1/auth/login", { paymail, password }, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    const response = await axios.post(
+      "https://api.kipaji.app/api/v1/auth/login",
+      { paymail, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     console.log(response.data);
 
     if (response.data) {
-      return { status: 200, token: response.data.token, secretKey: response.data.secretKey, username: response.data.username }; 
+      return {
+        status: 200,
+        token: response.data.token,
+        secretKey: response.data.secretKey,
+        username: response.data.username,
+      };
     } else {
-      return { status: 404, data: { code: "404", Message: "Not Found" } }; 
+      return { status: 404, data: { code: "404", Message: "Not Found" } };
     }
   } catch (error) {
-    console.error('Error making Axios request:', error);
-    return { status: 500, data: { code: "5000", Message: "Internal Server Error" } }; 
+    console.error("Error making Axios request:", error);
+    return {
+      status: 500,
+      data: { code: "5000", Message: "Internal Server Error" },
+    };
   }
 }
 
@@ -101,11 +115,13 @@ router.post("/create", async (req, res) => {
   }
 });
 
-router.post('/login-user', async (req, res) => {
+router.post("/login-user", async (req, res) => {
   const { paymail, password } = req.body;
 
   if (!paymail || !password) {
-    return res.status(400).json({ code: "2000", Message: "Error in obtaining details" });
+    return res
+      .status(400)
+      .json({ code: "2000", Message: "Error in obtaining details" });
   }
 
   const result = await loginSuccess(paymail, password);
@@ -131,21 +147,23 @@ router.post("/", async (req, res) => {
       twiml.message("Please provide your preferred username.");
       user.step = 1;
     } else if (incomingMessage === "log in") {
-      twiml.message(`Please provide your paymail and password through the link ${userLink}`);
+      twiml.message(
+        `Please provide your paymail and password through the link ${userLink}`
+      );
 
       const checkStatusInterval = setInterval(async () => {
-      //  console.log(`Checking loginStatus for ${fromNumber}:`, loginStatus);
+        //  console.log(`Checking loginStatus for ${fromNumber}:`, loginStatus);
 
-       const status = loginStatus["undefined"]?.status
+        const status = loginStatus["undefined"]?.status;
         // console.log("The correct status is:", status)
-        const userName = loginStatus["undefined"]?.username
+        const userName = loginStatus["undefined"]?.username;
         // console.log("The guys name is:", userName)
 
         if (status === 200) {
           clearInterval(checkStatusInterval);
           await client.messages.create({
             body: `Hello ${userName}, Welcome back. To proceed, please choose:\n1. Initiate a transaction\n2. Deposit\n3. Withdraw`,
-            from: "whatsapp:+14155238886", 
+            from: "whatsapp:+14155238886",
             to: fromNumber,
           });
 
@@ -154,7 +172,7 @@ router.post("/", async (req, res) => {
           clearInterval(checkStatusInterval);
           await client.messages.create({
             body: "Invalid paymail or password, Please try again",
-            from: "whatsapp:+14155238886", 
+            from: "whatsapp:+14155238886",
             to: fromNumber,
           });
           user.step = 0;
@@ -180,8 +198,10 @@ router.post("/", async (req, res) => {
     const response = await chainRequest(user.name);
 
     if (response === 201) {
-      user.key = Date.now(); 
-      twiml.message(`Click this link to continue: ${sendLink}, You have 5 minutes`);
+      user.key = Date.now();
+      twiml.message(
+        `Click this link to continue: ${sendLink}, You have 5 minutes`
+      );
 
       setTimeout(async () => {
         try {
@@ -190,14 +210,14 @@ router.post("/", async (req, res) => {
           if (key) {
             await client.messages.create({
               body: "Welcome aboard, you have successfully joined Benkiko. To proceed reply with Log in",
-              from: "whatsapp:+14155238886", 
+              from: "whatsapp:+14155238886",
               to: fromNumber,
             });
-            user.step = 0
+            user.step = 0;
           } else {
             await client.messages.create({
               body: "Sorry, an error was encountered in registering you.",
-              from: "whatsapp:+14155238886", 
+              from: "whatsapp:+14155238886",
               to: fromNumber,
             });
             user.step = 0;
@@ -206,7 +226,7 @@ router.post("/", async (req, res) => {
           console.error("Error during validateLogin:", error);
           await client.messages.create({
             body: "Account creation isn't successful.",
-            from: "whatsapp:+14155238886", 
+            from: "whatsapp:+14155238886",
             to: fromNumber,
           });
           user.step = 0;
@@ -217,74 +237,133 @@ router.post("/", async (req, res) => {
       user.step = 0;
     }
   } else if (user.step === 4) {
-    if (incomingMessage === "initiate a transaction" || incomingMessage === "1") {
+    if (
+      incomingMessage === "initiate a transaction" ||
+      incomingMessage === "1"
+    ) {
       twiml.message("Please provide the recipient's username.");
       user.step = 5;
+    } else if (incomingMessage === "Deposit" || incomingMessage === "2") {
+      twiml.message(
+        "Great, lets get on with depositing some assets. What assets do you want to deposit?"
+      );
+      user.step = 8;
     } else {
-      twiml.message('Please choose:\n1. Initiate a transaction\n2. Deposit\n3. Withdraw');
+      twiml.message(
+        "Please choose:\n1. Initiate a transaction\n2. Deposit\n3. Withdraw"
+      );
     }
   } else if (user.step === 5) {
     const recipient = incomingMessage;
-    recipientName = recipient
+    recipientName = recipient;
 
     if (recipientName) {
-      twiml.message("Provide the asset you wish to transact with")
+      twiml.message("Provide the asset you wish to transact with");
       user.step = 6;
     } else {
       twiml.message("Recipient username is required.");
       user.step = 4;
     }
-  } else if(user.step === 6){
+  } else if (user.step === 6) {
     const asset = incomingMessage;
     assetTransct = asset;
 
     if (assetTransct) {
-      twiml.message("Provide the amount you wish to transact")
+      twiml.message("Provide the amount you wish to transact");
       user.step = 7;
     } else {
       twiml.message("An amount id required to continue is required.");
       user.step = 5;
     }
-
-    
   } else if (user.step === 7) {
-    const amount = incomingMessage
-    amountTransct = amount
+    const amount = incomingMessage;
+    amountTransct = amount;
 
     if (recipientName && assetTransct && amountTransct) {
-      console.log(`The name-----> ${recipientName}, the asset-----> ${assetTransct}, the amount-----> ${amountTransct}`)
-      
+      console.log(
+        `The name-----> ${recipientName}, the asset-----> ${assetTransct}, the amount-----> ${amountTransct}`
+      );
+
       try {
         const loginData = loginStatus["undefined"];
-        console.log("This is the login data:",loginData)
+        console.log("This is the login data:", loginData);
         if (loginData && loginData.token && loginData.secretKey) {
-          const paymentResponse = await initPayment(recipientName, assetTransct, amountTransct, loginData.token, loginData.secretKey);
+          const paymentResponse = await initPayment(
+            recipientName,
+            assetTransct,
+            amountTransct,
+            loginData.token,
+            loginData.secretKey
+          );
 
-          if(paymentResponse.code === 201){
+          if (paymentResponse.code === 201) {
             twiml.message(`Transaction to ${recipientName} was successful.`);
           } else {
-            twiml.message("Operational error during transaction please repeat the process")
+            twiml.message(
+              "Operational error during transaction please repeat the process"
+            );
           }
-          
         } else {
           twiml.message("Login data is missing. Please log in again.");
           user.step = 0;
         }
       } catch (error) {
         console.error("Error during transaction:", error);
-        twiml.message("An error occurred during the transaction. Please try again.");
+        twiml.message(
+          "An error occurred during the transaction. Please try again."
+        );
         user.step = 4;
       }
     } else {
       twiml.message("Recipient username is required.");
       user.step = 4;
     }
+  } else if (user.step === 8) {
+    const changeAsset = incomingMessage;
+    console.log(changeAsset)
+    reqChangeAsset = changeAsset;
+    console.log("The passed asset------>", reqChangeAsset);
 
+    if (reqChangeAsset) {
+      try {
+        const loginData2 = loginStatus["undefined"];
+        const depoUrl =
+          "https://staging.api.benkiko.io/v1/transactions/deposit/interactive";
+        if (loginData2 && loginData2.secretKey) {
+          const depositHandle = await withdrawDeposit(
+            reqChangeAsset,
+            depoUrl,
+            loginData2.secretKey
+          );
+          // console.log("The data am supposed to receive------------->",depositHandle);
+
+          if (depositHandle.code === 200) {
+            twiml.message(
+              `Navigate to this URL to complete the deposit process:\n${depositHandle.url}`
+            );
+          } else {
+            twiml.message(
+              "Operational error occured. Please repeat the process"
+            );
+            user.step = 4;
+          }
+        }
+      } catch (error) {
+        console.error("Error during transaction:", error);
+        twiml.message(
+          "An error occurred during the transaction. Please try again."
+        );
+        user.step = 4;
+      }
+    } else {
+      twiml.message("Recipient username is required.");
+      user.step = 4;
+    }
   }
   res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
 });
 
-initMessage()
+initMessage();
 
 module.exports = router;
---
+++ WithdrawDeposit.js
@@ -0,0 +1,71 @@
+const { getToken } = require("./getToken.js");
+const axios = require("axios");
+
+let reqAuth = null;
+
+const withdrawDeposit = async (reqChangeAsset, depoUrl, secretKey) => {
+  try {
+    const obAuth = await getToken();
+    reqAuth = obAuth.data.token;
+    // console.log(reqAuth);
+
+    const asset = String(reqChangeAsset).toLocaleUpperCase();
+    const aqSecretK = String(secretKey)
+
+    // console.log(`The asset is ${asset} and the secret key is as -------> ${aqSecretK}`)
+
+    const data = {
+      asset_code: asset,
+      limit: "922337203685.4775807",
+      secret_key: aqSecretK,
+    };
+
+    const responseChange = await axios.post(
+      "https://staging.api.benkiko.io/v1/change-trust",
+      data,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${reqAuth}`,
+        },
+      }
+    );
+
+    if (responseChange.data) {
+      const responseDeposit = await axios.get(depoUrl,
+        {
+          params: {
+            asset_code: asset,
+            domain: "testanchor.stellar.org",
+            secret_key: aqSecretK,
+          },
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${reqAuth}`,
+          },
+        }
+      );
+
+      if (responseDeposit.data) {
+        const sucCode = responseDeposit.data.code
+        const sucData = responseDeposit.data.data
+        const sucUrl = responseDeposit.data.data.url
+        const sucId = responseDeposit.data.data.id
+
+       
+        return { code: sucCode, data: sucData, url: sucUrl, id: sucId }
+      } else {
+        console.log("Unsuccessful transaction");
+        return { code: 400, message: "Error occurred" };
+      }
+    } else {
+      console.log("Unsuccessful transaction");
+      return { code: 400, message: "Error occurred" };
+    }
+  } catch (axiosError) {
+    console.error("Axios error:", error.message);
+    return { code: 500, message: "Internal server error" };
+  }
+};
+
+module.exports = { withdrawDeposit };
